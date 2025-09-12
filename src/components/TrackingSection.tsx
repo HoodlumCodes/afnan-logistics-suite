c6daecbc50001b35a22ebe49850d6c9f4e022271
HoodlumCodes <hoodlumwoods@gmail.com>
2025-09-12 15:44:24 +0100

    modified:   src/components/TrackingSection.tsx

--- src/components/TrackingSection.tsx
+++ src/components/TrackingSection.tsx
@@ -32,8 +32,7 @@ const TrackingSection = () => {
     { status: "In Transit", location: "Luxembourg", timestamp: "Sept 12, 2025 02:22", description: "Package has reached Luxembourg and is preparing for transfer toward Belgium.", icon: Truck, completed: true},
     { status: "In Transit", location: "Luxembourg → Belgium", timestamp: "Sept 12, 2025 09:50", description: "Package has departed Luxembourg and is currently in transit to Belgium.", icon: Truck, completed: true},
     { status: "In Transit", location: "Bruxelles, Belgium", timestamp: "Sept 12, 2025 13:41", description: "Package is currently in Bruxelles, Belgium and is in transit to the Belgium–France–UK border.", icon: Truck, completed: true},
-
-    // { status: "Border Clearance", location: "Belgium–France–UK Border", timestamp: "Sept 12, 2025 17:19", description: "Package is held at the Belgium, France, and UK border for customs clearance, insurance verification, and final paperwork.", icon: Truck, completed: false},
+    { status: "Border Clearance", location: "Belgium–France–UK Border", timestamp: "Sept 12, 2025 17:42", description: "Package is held at the Belgium, France, and UK border for customs clearance, insurance verification, and final paperwork.", icon: Truck, completed: false},
   ],
 };
 
